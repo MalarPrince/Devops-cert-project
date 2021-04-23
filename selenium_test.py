abdfee2eb0486697ef7584c0e63e2054a93df219
MalarPrince <82498963+MalarPrince@users.noreply.github.com>
2021-04-23 13:29:01 +0530

    Update selenium_test.py

--- selenium_test.py
+++ selenium_test.py
@@ -25,7 +25,7 @@ def browser():
 
 # Test step 1 - Open URL
 def test_open_url(browser):
-    browser.get("http://35.154.147.222/")
+    browser.get("http://18.221.130.251:80/")
 
 
 # Test step 2 - Check Title
